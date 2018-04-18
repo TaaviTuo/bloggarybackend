c5015c368e314baa6c9e3d91a93c36bf29aa749d
Taavi Tuomela <31739071+TaaviTuo@users.noreply.github.com>
2018-04-18 12:13:31 +0300

    Redid major parts of the controller and added methods to add users and comments.

--- MyRestController.java
+++ MyRestController.java
@@ -2,16 +2,17 @@ package fi.tamk.bloggarybackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.PostConstruct;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 public class MyRestController {
+
+    private final String HTTP_HEADER_ID = "1";
+
 
     @Autowired
     BlogPostHandler blogPostHandler;
@@ -23,46 +24,76 @@ public class MyRestController {
         BlogPost test = blogPostHandler.saveEntity(new BlogPost("Testiä", "Testitesti lul", new User()));
     }
 
-    @RequestMapping(value = "/blogPosts")
-    public Iterable getAll() {
+    public MyRestController(){
 
+    }
+
+    //WORK IN PROGRESS
+    @RequestMapping(path = "/adduser", method = RequestMethod.POST) // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestBody User user, @RequestHeader HttpHeaders headers) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        if (checkHeaders(headers)){
+            //userRepository.save(user);
+            return "User Saved";
+        }
+        return "User not Saved";
+    }
+
+    @RequestMapping(path = "/addpost", method = RequestMethod.POST) // Map ONLY POST Requests
+    public @ResponseBody String addNewThread (@RequestBody BlogPost post, @RequestHeader HttpHeaders headers) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        if (checkHeaders(headers)){
+            blogPostHandler.saveEntity(post);
+            return "Blog post Saved";
+        }
+        return "Blog post not Saved";
+    }
+
+    @RequestMapping(path = "/addcomment", method = RequestMethod.POST) // Map ONLY POST Requests
+    public @ResponseBody String addNewMessage (@RequestBody String comment, @RequestParam String id, @RequestHeader HttpHeaders headers) {
+        // Change String in RequestBody to Comment when they work
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        if (checkHeaders(headers)){
+            //MessageThread tmp = messageThreadRepository.findOne(id);
+            //tmp.getListOfMessages().add(msg);
+            //messageThreadRepository.save(tmp);
+            //msgRepository.save(m);
+           return "CommentSaved";
+        }
+        return "Comment not Saved";
+    }
+
+    @RequestMapping(path = "/blogposts/{userId}", method = RequestMethod.GET)
+    public @ResponseBody BlogPost getPost(@PathVariable int userId, Long id) {
+        return blogPostHandler.findOne(id);
+    }
+
+    @GetMapping(path="/blogposts")
+    public @ResponseBody Iterable<BlogPost> getAllPosts() {
+        // This returns a JSON or XML with the users
         return blogPostHandler.findAll();
     }
 
-    @RequestMapping(value = "/blogPosts/{userId}")
-    public BlogPost getOne(@PathVariable int userId, Long id) {
+    //@GetMapping(path="/allusers")
+    //public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+    //    return userRepository.findAll();
+    //}
 
-        if (blogPostHandler.findOne(id) != null) {
+    //@RequestMapping(path = "/user", method = RequestMethod.GET)
+    //public @ResponseBody User getUser(@RequestParam(value="id", defaultValue="") Long id) {
+    //    return userRepository.findOne(id);
+    //}
 
-            return blogPostHandler.findOne(id);
-        } else {
-
-            throw new MyBlogPostException(userId);
+    public boolean checkHeaders(HttpHeaders headers){
+        if (headers.containsKey("app_id")){
+            if (headers.get("app_id").contains(HTTP_HEADER_ID)){
+                return true;
+            }
         }
-    }
-
-    @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public ResponseEntity<Void> addBlogPost(@RequestBody BlogPost toAdd, UriComponentsBuilder b) {
-
-        blogPostHandler.saveEntity(toAdd);
-
-        UriComponents uriComponents = b.path("/blogPosts/{userId}").buildAndExpand(toAdd.getId());
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(uriComponents.toUri());
-
-        return new ResponseEntity<>(headers, HttpStatus.CREATED);
-    }
-
-    @RequestMapping(value = "/delete")
-    public void deleteBlogPost(@PathVariable int userId, Long toDelete) {
-
-        if (blogPostHandler.findOne(toDelete) != null) {
-
-            blogPostHandler.delete(toDelete);
-        } else {
-
-            throw new MyBlogPostException(userId);
-        }
+        return false;
     }
 }
